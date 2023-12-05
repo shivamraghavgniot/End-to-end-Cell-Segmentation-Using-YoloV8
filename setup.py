761391715d9c61b3f15b7d920664b5190f1011df
shivamraghavgniot <shivam.raghav.779@gmail.com>
2023-12-05 15:20:43 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Shivam Raghav',
+    author_email= 'shivam.raghav.779@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
